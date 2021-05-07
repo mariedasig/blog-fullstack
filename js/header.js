39e8d75407a342ac538e40a438487e5e6a9d9c73
Marie Dasig <mariedasig@gmail.com>
2021-05-06 21:21:56 -0700

    first commit

---
+++ js/header.js
@@ -0,0 +1,8 @@
+header.innerHTML = `<header class="my-header">
+<div class="logo">LOGO</div>
+<div>
+ <a class="my-header-link" href="about.html">About</a>
+ <input type="text" />
+ <button>Search</button>
+</div>
+</header>`;
